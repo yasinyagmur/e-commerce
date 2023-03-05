dbda6ef622ed06af1032fb405ae78a3c4c36bd57
yasin yağmur <yasin.yagmur@gmail.com>
2023-03-06 00:02:13 +0300

    card component created

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -7,13 +7,14 @@ const Card = ({ item }) => {
     <Link className="link" to={`/product/${item.id}`}>
       <div className="card">
         <div className="image">
+          {item.isNew && <span>New Season</span>}
           <img src={item.img} alt="" className="mainImg" />
           <img src={item.img2} alt="" className="secondImg" />
         </div>
         <h2>{item.title}</h2>
         <div className="prices">
-          <h3> {item.oldPrice} </h3>
-          <h3> {item.price} </h3>
+          <h3>${item.oldPrice} </h3>
+          <h3>${item.price} </h3>
         </div>
       </div>
     </Link>
--- src/Components/FeaturedProducts/FeaturedProducts.jsx
+++ src/Components/FeaturedProducts/FeaturedProducts.jsx
@@ -21,6 +21,23 @@ const FeaturedProducts = ({ type }) => {
       oldPrice: 19,
       price: 12,
     },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgbw=1600",
+      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgbw=1600",
+      title: "Long Sleeve",
+      isNew: false,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 4,
+      img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgbw=1600",
+      title: "Coat",
+      isNew: false,
+      oldPrice: 19,
+      price: 12,
+    },
   ];
   return (
     <div className="featuredProducts">
