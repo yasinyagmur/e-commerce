d23fdbc449d278df9f5f463ab46c110280fa1dc9
yasin yağmur <yasin.yagmur@gmail.com>
2023-03-05 23:49:43 +0300

    featured component created

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import FeaturedProducts from "../../Components/FeaturedProducts/FeaturedProducts";
 import Slider from "../../Components/Slider/Slider";
 import "./Home.scss";
 const Home = () => {
   return (
     <div className="home">
       <Slider />
+      <FeaturedProducts type="featured" />
+      <FeaturedProducts type="trending" />
     </div>
   );
 };
